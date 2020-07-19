d81b67be02c5018610cc5dcbc7678ecb9a7a90f5
cownectar <josephparent613@gmail.com>
2020-07-19 20:04:23 +1000

    Finished remember me

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,22 +4,43 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-	def log_out
-		reset_session
-		#Security related
-		@current_user = nil
-	end
+  def remember(user)
+	user.remember
+	#permanent.signed encrypts the permanent cookies
+	cookies.permanent.signed[:user_id] = { value: user.id }
+	cookies.permanent[:remember_token] = user.remember_token
+  end
 
-	def current_user
-		# Returns the current logged-in user (if any).
-		if session[:user_id]
-			## This works the same as @current_user = @current_user || User.find_by(id: session[:user_id]), where the first expression is evaluated, and if it turns nil (AKA hasn't run before, the executes User.find_by...)
-			@current_user ||= User.find_by(id: session[:user_id])
+#Forgets a persistant session
+def forget(user)
+	user.forget
+  cookies.delete(:user_id)
+  cookies.delete(:remember_token)
+end
+
+def log_out
+	forget(current_user)
+	reset_session
+	#Security related
+	@current_user = nil
+end
+
+def current_user
+	# Returns the current logged-in user (if any).
+	if (user_id = session[:user_id]) #This is assignment not comparison
+		## This works the same as @current_user = @current_user || User.find_by(id: session[:user_id]), where the first expression is evaluated, and if it turns nil (AKA hasn't run before, the executes User.find_by...)
+		@current_user ||= User.find_by(id: user_id)
+	elsif cookies.permanent.signed[:user_id] #Checks if its true aka exists
+		user = User.find_by(id: cookies.signed[:user_id])
+		if user && user.authenticated?(cookies[:remember_token])
+			log_in user
+			@current_user = user
 		end
 	end
-	
-	def logged_in?
-		#Bang negates it, so "If the user is not NOT logged"
-		!current_user.nil?
-	end
+end
+
+def logged_in?
+	#Bang negates it, so "If the user is not NOT logged"
+	!current_user.nil?
+end
 end
