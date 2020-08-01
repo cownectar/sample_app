55eeec50daafe56ad05074669dced83bc529da09
cownectar <josephparent613@gmail.com>
2020-08-01 17:42:52 +1000

    Another mailgun attempt -.-

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,14 @@ Rails.application.configure do
 
   
   #config.action_mailer.raise_delivery_errors = false
-  host = 'https://glacial-sands-21738.herokuapp.com/'
+  host = 'glacial-sands-21738.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'glacial-sands-21738.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
