8dc6eba6780f0896865d664fca8c4a8d982f4c84
cownectar <josephparent613@gmail.com>
2020-08-02 13:51:40 +1000

    Maybe this time..

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :authentication => :plain,
   }
-  ActionMailer::Base.delivery_method = :smtp
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
