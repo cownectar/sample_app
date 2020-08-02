970442bb276410e8ed19722060632c985ac2f819
cownectar <josephparent613@gmail.com>
2020-08-02 11:01:12 +1000

    Try # 376476

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'glacial-sands-21738.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
