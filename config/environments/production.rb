407648e299b1e0bb4c6a35b0b5acbdcfa515056d
cownectar <josephparent613@gmail.com>
2020-08-02 14:12:00 +1000

    Mailer change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   
   config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'glacial-sands-21738.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
@@ -75,7 +75,7 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :authentication => :plain,
   }
-
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
