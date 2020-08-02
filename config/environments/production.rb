d1205c54b7ff44e8b01c97ca6b0eb97ff3f120f0
cownectar <josephparent613@gmail.com>
2020-08-02 14:33:56 +1000

    Another one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,10 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'glacial-sands-21738.herokuapp.com/'
+  host = 'glacial-sands-21738.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
